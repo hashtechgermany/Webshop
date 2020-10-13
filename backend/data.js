d4bf7d6f0751ecf941eaff967d9d905b713c0ffe
hashtechgermany <72353541+hashtechgermany@users.noreply.github.com>
2020-10-13 11:07:16 +0200

    Created Node Express Server
    Use Node Express Server as Backend Server

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+export default {
+products:[
+    {
+    _id:'11',
+    name: 'Slim Shirt',
+    category:'Shirts',
+    image: '/images/d1.jpg',
+    price: 60,
+    brand: 'North Face',
+    rating: 4.5,
+    numReviews:10
+},
+{
+    _id:'12',
+    name: 'Fit Shirt',
+    category:'Shirts',
+    image: '/images/d1.jpg',
+    price: 50,
+    brand: 'Crocodile',
+    rating: 4.2,
+    numReviews:8
+},
+{
+    _id:'13',
+    name: 'Best Pants',
+    category:'Pants',
+    image: '/images/d1.jpg',
+    price: 70,
+    brand: 'Raymonds',
+    rating: 3.5,
+    numReviews:35
+},
+{
+    _id:'14',
+    name: 'Narrow Pants',
+    category:'Pants',
+    image: '/images/d1.jpg',
+    price: 49,
+    brand: 'Canada',
+    rating: 3.9,
+    numReviews:18
+},
+{
+    _id:'15',
+    name: 'Modern Fit',
+    category:'Shirts',
+    image: '/images/d1.jpg',
+    price: 30,
+    brand: 'Bugatti',
+    rating: 4.5,
+    numReviews:12
+}
+]
+}
